90caf7bf0c160ded9ebe3d13ae27722daf1bf542
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-01-10 01:10:47 -0600

    Change to Beige

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -20,7 +20,7 @@ export default function Modal({ open, setOpen, title, description }: Props) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-beige bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
